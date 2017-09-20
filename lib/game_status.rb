f9edf8b04acf69721f67e3d0c9bad623d74af216
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-20 20:26:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,4 +9,5 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 def won?(board)
   WIN_COMBINATIONS.detect? do |combo|
     combo.all? { |index| board[index] == "X" } || combo.all? { |index| board[index] == "O" }
-      
+  end
+end
